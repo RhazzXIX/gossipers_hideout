a4b7146c35b1dded385f76aeb2f3c2b9aa07957c
RhazzXIX <jm21tonio@gmail.com>
2023-07-02 01:09:17 +0800

    Set up initial files and dependencies

---
+++ app.ts
@@ -0,0 +1,44 @@
+import express, { ErrorRequestHandler } from "express";
+import createError from "http-errors";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import logger from "morgan";
+require("dotenv").config();
+const path = require("path");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+} as ErrorRequestHandler);
+
+module.exports = app;
