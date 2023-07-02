d6c3058ffd3cc619e13899c157e6052a7e17e431
RhazzXIX <jm21tonio@gmail.com>
2023-07-02 08:18:45 +0800

    Create authentication config & add passport to app

--- app.ts
+++ app.ts
@@ -1,16 +1,29 @@
 import express, { ErrorRequestHandler } from "express";
 import createError from "http-errors";
 import compression from "compression";
-import cookieParser from "cookie-parser";
 import session from "express-session";
 import logger from "morgan";
+import mongoose, { ConnectOptions } from "mongoose";
+import passport from "./config/authentication";
 require("dotenv").config();
 const path = require("path");
-
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+const mongoDb = process.env.MONGODB_URI || "";
+
+const main = async () => {
+  await mongoose.connect(mongoDb, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  } as ConnectOptions);
+  console.log("Connected to database");
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "Mongo connection error"));
+};
+
+main().catch((err) => console.log(err));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -19,7 +32,22 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SECRET_KEY!,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
---
+++ config/authentication.ts
@@ -0,0 +1,50 @@
+import passport from "passport";
+import passportLocal from "passport-local";
+import bcrypt from "bcryptjs";
+
+const User = require("../models/user");
+
+declare global {
+  namespace Express {
+    interface User {
+      _id: string;
+    }
+  }
+}
+
+const LocalStrategy = passportLocal.Strategy;
+
+passport.use(
+  new LocalStrategy(async function verify(username, password, done) {
+    try {
+      const user = await User.findOne({ name: username }).exec();
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, function (err, res) {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.finById(id).exec();
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+export default passport;
